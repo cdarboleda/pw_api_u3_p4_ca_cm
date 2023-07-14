4d4c6bc11c302e9672fb20877396105c79cf0261
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-07-13 19:49:43 -0500

    Taller 28

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
@@ -23,10 +28,15 @@ public class EstudianteControllerRestFul {
 	private IEstudianteService estudianteService;
 	
 	//GET
-	@GetMapping(path="/buscar")
-	public Estudiante consultarPorCedula() {
-		String cedula = "1234567890";
+	@GetMapping(path="/buscar/{cedula}")
+	public Estudiante consultarPorCedula(@PathVariable String cedula) {
 		return this.estudianteService.consultarPorCedula(cedula);
+	}
+	
+	@GetMapping(path="/buscarTodos")
+	public List<Estudiante> buscarTodos(@RequestParam String provincia){
+		//buscarTodos?provincia=pichincha
+		return this.estudianteService.buscarTodos();
 	}
 	
 	//POST
@@ -36,21 +46,29 @@ public class EstudianteControllerRestFul {
 	}
 	
 	//PUT
-	@PutMapping(path="/actualizar")
-	public void actualizar() {
-		
+	@PutMapping(path="/actualizar/{identificador}")
+	public void actualizar(@RequestBody Estudiante estudiante, @PathVariable Integer identificador) {
+		estudiante.setId(identificador);
+		this.estudianteService.actualizar(estudiante);
 	}
 	
 	//PATCH
-	@PutMapping(path="/actualizarParcial")
-	public void actualizarParcial() {
-		
+	@PatchMapping(path="/actualizarParcial/{identificador}/{cedula}")
+	public void actualizarParcial(@RequestBody Estudiante estudiante,
+			@PathVariable Integer identificador,
+			@PathVariable String cedula) {
+		estudiante.setId(identificador);
+		Estudiante estu1 = this.estudianteService.consultarPorCedula(cedula);
+		estu1.setCedula(estudiante.getCedula());
+		this.estudianteService.actualizar(estu1);
 	}
 	
 	//DELETE
-	@DeleteMapping(path="/borrar")
-	public void borrar() {
-		
+	@DeleteMapping(path="/borrar/{id}")
+	public void borrar(@PathVariable Integer id) {
+		this.estudianteService.eliminar(id);
 	}
+	
+
 
 }
--- src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
@@ -1,11 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.repository.modelo.Estudiante;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
@@ -30,6 +33,41 @@ public class EstudianteRepositoryImpl implements IEstudianteRepository{
 	public void insertar(Estudiante estudiante) {
 		// TODO Auto-generated method stub
 		this.entityManager.persist(estudiante);
+	}
+
+	@Override
+	public void actualizar(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		this.entityManager.merge(estudiante);
+	}
+
+	@Override
+	public void actualizarParcial(String cedulaActual, String cedulaNueva) {
+		// TODO Auto-generated method stub
+		String sql = "UPDATE Estudiante e SET e.cedula = :datoCedula WHERE e.cedula = :datoCondicion ";
+		Query myQuery = this.entityManager.createQuery(sql);
+		myQuery.setParameter("datoCondicion", cedulaActual);
+		myQuery.setParameter("datoCedula", cedulaNueva);
+		myQuery.executeUpdate();
+	}
+
+	@Override
+	public void borrar(Integer id) {
+		// TODO Auto-generated method stub
+		this.entityManager.remove(this.buscarPorId(id));
+	}
+
+	@Override
+	public Estudiante buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return this.entityManager.find(Estudiante.class, id);
+	}
+
+	@Override
+	public List<Estudiante> seleccionarTodos() {
+		// TODO Auto-generated method stub
+        TypedQuery<Estudiante> myQuery = this.entityManager.createQuery("SELECT e FROM Estudiante e", Estudiante.class);
+        return myQuery.getResultList();
 	}
 	
 	
